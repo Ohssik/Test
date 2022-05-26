ea137afc2828df840cbf9abbddef7385e278972c
Ohssik <92903646+Ohssik@users.noreply.github.com>
2022-05-26 17:41:16 +0800

    Add files via upload

---
+++ BasePage.aspx.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+using System.Web.Configuration;
+using System.Collections;
+using System.Configuration;
+
+
+    public partial class BasePage : System.Web.UI.Page
+    {
+        public BasePage() { }
+        private void SetActionStamp()
+        {
+            Session["actionStamp"] = Server.UrlEncode(DateTime.Now.ToString());
+        }
+        void Page_PreRender(object sender, EventArgs e)
+        {
+            if (!IsPostBack)
+            {
+                SetActionStamp();
+            }
+			if(Session["actionStamp"]!=null)
+				ClientScript.RegisterHiddenField("actionStamp", Session["actionStamp"].ToString());
+			else
+				//ClientScript.RegisterHiddenField("actionStamp", Server.UrlEncode(DateTime.Now.ToString()));
+				Response.Redirect(Request.FilePath);
+        }
+        /// 取得值，指出網頁是否經由重新整理動作回傳 (PostBack)
+        /// </summary>
+        protected bool IsRefresh
+        {
+            get
+            {
+                if (HttpContext.Current.Request["actionStamp"] as string == Session["actionStamp"] as string)
+                {
+                    SetActionStamp();
+                    return false;
+                }
+
+                return true;
+            }
+        }
+        
+    }
